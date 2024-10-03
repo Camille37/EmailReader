550412ab56b482a74c342e2df31398a074625074
Mathias Peuch <mpeuch@enssat.fr>
2024-10-03 18:44:33 +0200

    Front-end for the article edition

--- src/app/article-edition/article-edition.component.ts
+++ src/app/article-edition/article-edition.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Category } from '../interfaces/category';
 @Component({
   selector: 'app-article-edition',
   templateUrl: './article-edition.component.html',
   styleUrl: './article-edition.component.css'
 })
 export class ArticleEditionComponent {
+  listCategory: any = Object.values(Category).filter(
+    (value): value is any => typeof value === 'string'
+  );
+  
+  browse(){
+    // To complete (load a picture)
+  }
 
+  save(){
+    // To complete (save the edition)
+  }
+
+  back(){
+    // To complete (back to the list)
+  }
 }
