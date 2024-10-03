598b7812c1ead3919b3e2f7e53697544414c3bf6
CamilleA <123865377+Camille37@users.noreply.github.com>
2024-10-04 00:09:39 +0200

    See all the article + can delete them

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,19 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../interfaces/category';
+import { NewsService } from '../services/news.service';
+import { LoginService } from '../services/login.service';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.css'
 })
-export class ArticleListComponent {
-  activeTab: Category = Category.All; // active onglet by default;
-  tabs : any = Object.values(Category).filter(
-    (value): value is any => typeof value === 'string'
-  );
-  
-    setActiveTab(tab: Category) {
-      this.activeTab = tab; // update the active onglet in the menu
+export class ArticleListComponent implements OnInit{
+
+  activeTab: Category = Category.All; // active tab by default;
+  tabs : any = Object.values(Category); // list of tabs
+
+  newsSvr : NewsService;
+  articleList : Article[] = [];
+
+  constructor(private newsSrv : NewsService, private loginSrv : LoginService){
+    if (!loginSrv.isLogged()){
+      newsSrv.setAnonymousApiKey();
     }
+    this.newsSvr = newsSrv;
+  }
+
+  ngOnInit() {
+    this.getArticles();
+  }
+  
+  setActiveTab(tab: Category) {
+    this.activeTab = tab; // update the active onglet in the menu
+  }
+
+  delete(article : Article){
+    const confirmed = window.confirm('Are you sure you want to delete '+ article.title +' ?');
+    if (confirmed){
+      this.newsSrv.deleteArticle(parseInt(article.id)).subscribe(
+        (data: Article) => {
+          const deletedArticle = data;
+          console.log(deletedArticle);
+        }
+      );
+      window.confirm(article.title +' have beeing deleted');
+      this.getArticles();
+    }
+   
+  }
+
+  getArticles(){
+    this.newsSrv.getArticles().subscribe(
+      (data: Article[]) => {
+        this.articleList = data;
+      },
+      error => {
+        console.error('Erreur lors de la récupération des articles:', error);
+      }
+    );
+  }
   
 }
